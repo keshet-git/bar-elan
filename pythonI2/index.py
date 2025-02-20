8d410eb8443058be2f039a090c9c79dc3a441852
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-20 23:07:27 +0200

    Update index.py

--- pythonI2/index.py
+++ pythonI2/index.py
@@ -1,18 +1,7 @@
 def my_reverse(num):
     num_as_str=str(num)
-    num_as_str=num_a_str[::-1]
+    num_as_str=num_as_str[::-1]
     return int(num_as_str)
 
-num=int(input("enter 3 numbers"))
-num2=my_reverse(num)
-print("reeversed:",num2)
-print(type(num2))
-
-'''
-num=int(input("enter 3 numbers"))
-x = [num]
-def num_torn():
-    l=x[::-1]
-
-print(l)
-'''
+num=int(input("enter 3 number: "))
+print("Reeversed:",my_reverse(num))
