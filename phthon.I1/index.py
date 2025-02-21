330c163d617cecae8db5dc60c7089c7afa8a0203
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-21 16:48:02 +0200

    Update index.py

--- phthon.I1/index.py
+++ phthon.I1/index.py
@@ -1,4 +1,3 @@
-
 def my_min(num1,num2,num3):
     if num2<num1:
         num1=num2
@@ -20,18 +19,4 @@ nums= input("Enter three numbers: ")
 l1=nums.split()
 x1,x2,x3=[int(x) for x in l1]
 print_max_min(x1,x2,x3)
-
-'''
-x1,x2,x3 =nums.split()
-minmal_num=my_min(x1,x2,x3)
-
-====================================
-    for x in range(y):
-        temp=temp*x
-    return temp
-
-w=f1(5,3)
-print(w)
-print(help(f1))
-'''
-# param_float*1.8 +32.0
+print(print_max_min)
