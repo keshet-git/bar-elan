48062fa5a541eb85cbb9fc3e4ade9e9baccd34ce
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-22 19:36:28 +0200

    Update ibsex.py

--- pythonB1/ibsex.py
+++ pythonB1/ibsex.py
@@ -8,7 +8,9 @@ height2=float(input("Enter your height: "))
 
 age_difference = age1-age2
 height_difference = round((height1-height2)*100)
-print("the name of the first person is:",name1,"the age of the first person is:",2024-age1)
-print("the name of the second person is:",name2,"the age of the first person is:",2024-age2)
+age1=age1-2024
+age2=age2-2024
+print("the name of the first person is:",name1,"the age of the first person is:",age1)
+print("the name of the second person is:",name2,"the age of the first person is:",age2)
 print("the age differenc is:" ,age_difference)
 print("the height differenc is:",height_difference)
