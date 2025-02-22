b89ac0b78ff85b74a8ff931b479eb1995d4308a3
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-22 20:56:54 +0200

    Update index.py

--- pythonD2/index.py
+++ pythonD2/index.py
@@ -1,13 +1,14 @@
 while True:
     name1=input("Enter your name1: ")
-    if name1=="End":
+    if name1=="end":
+        print("bay")
         break
     age1 =int(input("Enter your age1: "))
-    height1==int(input("Enter your heght1: "))
+    height1=float(input("Enter your height: "))
 
-    name2=input("Enter your name1: ")
-    age2 =int(input("Enter your age1: "))
-    height2==int(input("Enter your heght1: "))
+    name2=input("Enter your name2: ")
+    age2 =int(input("Enter your age2: "))
+    height2=float(input("Enter your height: "))
     if abs(age1-age2)<=5 and abs(height1-height2)<=5:
         print("There is a match")
     else:
