77e2d596c6f4321b21f43487d3cfbd3614cbf3d0
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-23 11:53:15 +0200

    Add files via upload

---
+++ str-dic/index.py
@@ -0,0 +1,10 @@
+data = 100
+print(type(data))
+data=[100,200,300]
+print(type(data))
+data=(100,200,300)
+print(type(data))
+print("Langth of tuuple is: ", len(data))
+data = {'Age': 1, 'Name': 2}
+print(type(data))
+print("Langth of tuuple is: ", len(data))
