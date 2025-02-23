9cd91f3941cc350745cde507881687ee8f59fc27
keshet-git <83539396+keshet-git@users.noreply.github.com>
2025-02-23 14:51:59 +0200

    Add files via upload

---
+++ big+smal/index.py
@@ -0,0 +1,11 @@
+l1=[]
+for i in range(10):
+    l1.append(int(input("enter a nmbers")))
+    
+l2=[]
+for i in range(10):
+    if "5" in str(l1[i]):
+        l2.append(i)
+print(len(str(12)))
+for idx in l2:
+    print(l1[idx])
